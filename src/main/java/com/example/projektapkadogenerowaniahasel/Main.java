b807ffa5d91b19227cf09adc7cc279ca911ed192
Nurek54 <g_sienkiewicz@o2.pl>
2023-06-03 16:40:37 +0200

    Końcowe poprawki

--- src/main/java/com/example/projektapkadogenerowaniahasel/Main.java
+++ src/main/java/com/example/projektapkadogenerowaniahasel/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("generator_view.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("Generator Haseł");
         primaryStage.setScene(new Scene(root, 400, 400));
